a0090a6996b22f138893821ada1058c0b5e230e1
Slava <106899177+Slava1804@users.noreply.github.com>
2023-10-19 13:57:38 +0600

    First commit

---
+++ frontend/routing.py
@@ -0,0 +1,22 @@
+from flask import *
+
+app = Flask(__name__)
+
+@app.route('/')
+def main():
+    return render_template('main.html')
+
+@app.route('/catalog')
+def catalog():
+    return render_template('catalog.html')
+
+@app.route('/basket')
+def basket():
+    return render_template('basket.html')
+
+@app.route('/item')
+def item():
+    return render_template('item.html')
+
+if __name__ == '__main__':
+    app.run(host='localhost', port=4200)
---
+++ frontend/static/js/base.js
@@ -0,0 +1,8 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById('sidebar');
+    if (sidebar.classList.contains('sidebar-hidden')) {
+        sidebar.classList.remove('sidebar-hidden');
+    } else {
+        sidebar.classList.add('sidebar-hidden');
+    }
+}
